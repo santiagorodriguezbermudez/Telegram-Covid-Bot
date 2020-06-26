f67f20ef90b7aa08f1f7d01dade3d4b06eb56867
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-06-25 23:51:43 -0500

    Added dotenv gem for private token

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,7 @@
 require 'telegram/bot'
+require 'dotenv'
 require_relative '../lib/bot.rb'
-token = '1007803026:AAGZrVhGGbPZmfvfc902Hdy4YXUa7hq-HU0'
-Bot.new(token)
+
+Dotenv.load
+Bot.new(ENV['token'])
 
